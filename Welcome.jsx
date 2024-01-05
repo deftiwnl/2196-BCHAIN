666b7bb237f6065825f39a712ad4e9ee5dbedaa3
daniel <123434131+deftiwnl@users.noreply.github.com>
2024-01-05 15:11:27 +0800

    Add files via upload

---
+++ Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    let message = "I think I've done it";
+    return(
+        <div>
+            {<h1>{message}</h1>}
+            <p>Welcome, I'm Daniel Fernandez</p>
+        </div>
+    );
+}
